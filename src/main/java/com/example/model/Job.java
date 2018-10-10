3ef7af04ab45833567db82b01d86d307a393aae8
jeffery <jeffery_feng@126.com>
2018-10-10 10:22:56 +0800

    创建fdh_test2库的job表

---
+++ src/main/java/com/example/model/Job.java
@@ -0,0 +1,23 @@
+package com.example.model;
+
+public class Job {
+    private Integer id;
+
+    private String jobname;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getJobname() {
+        return jobname;
+    }
+
+    public void setJobname(String jobname) {
+        this.jobname = jobname == null ? null : jobname.trim();
+    }
+}
